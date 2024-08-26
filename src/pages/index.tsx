e761077a6cbb5cbd392c091455132e8416c80050
Nasir6276 <92574698+Nasir6276@users.noreply.github.com>
2024-08-26 11:19:00 +0100

    image fetching url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,66 @@ import { IconStar } from "@tabler/icons-react";
 
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { Container, Flex, Text, Image, Button, Box } from "@mantine/core";
+import {
+  Container,
+  Flex,
+  Text,
+  Image,
+  Button,
+  Box,
+  Checkbox,
+  Radio,
+  Group,
+} from "@mantine/core";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [value, setValue] = useState("");
+  const [photo, setPhoto] = useState("");
+  const [photoId, setPhotoId] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const fetchImage = async () => {
+    try {
+      const { data: res } = await axios.get(
+        "http://47.236.63.61:4050/image",
+        {}
+      );
+      console.log(res);
+      setPhoto(res.image.url);
+      setPhotoId(res.image.id);
+    } catch (error) {}
+  };
+
+  const handleSubmit = async () => {
+    if (value.length < 1) {
+      alert("Select a type to proceed");
+      return;
+    }
+
+    setLoading(true);
+    try {
+      console.log(value);
+      const { data: res } = await axios.patch(
+        `http://47.236.63.61:4050/image/${photoId}`,
+        { type: value }
+      );
+      setValue("");
+      fetchImage();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchImage();
+  }, []);
+
   return (
     <>
       <Container size={1200}>
@@ -19,31 +74,32 @@ export default function Home() {
           gap={20}
         >
           <Box>
-            <Image
-              radius="md"
-              src="https://media.istockphoto.com/id/1269776313/photo/suburban-house.jpg?s=612x612&w=0&k=20&c=iNaSdrxJt7H37rjQZumXYScrmSTRm2fDJrqZzxpDL_k="
-              w={"100%"}
-              h={"100%"}
-            />
+            <Image radius="md" src={photo} w={"100%"} h={"100%"} alt="image" />
           </Box>
 
           <Container size={400}>
             <Text ta="center" fz={14} fw={500}>
-              "Please rate this image based on your preference. Your feedback
-              helps us improve!"
+              Please rate this image based on your preference. Your feedback
+              helps us improve!
             </Text>
           </Container>
-          <Flex align="center" justify="center" gap={40}>
-            <Button bg="#008625" c="#fff" w="200%" h={30}>
-              Indoor
-            </Button>
-            <Button bg="#008625" c="#fff" w="250%" h={30}>
-              Outdoor
-            </Button>
-          </Flex>
+          <Radio.Group name="selectType" value={value} onChange={setValue}>
+            <Group mt="xs">
+              <Radio value="indoor" label="Indoor" />
+              <Radio value="outdoor" label="Outdoor" />
+            </Group>
+          </Radio.Group>
 
           <Container size={400} w="100%">
-            <Button bg="#008625" c="#fff" fullWidth={true} w="100%" h={50}>
+            <Button
+              bg="#008625"
+              c="#fff"
+              fullWidth={true}
+              w="100%"
+              h={50}
+              onClick={handleSubmit}
+              loading={loading}
+            >
               Submit
             </Button>
           </Container>
